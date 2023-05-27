b9a6b47d690a699cff627c48b1f3dc7fa94b8e3c
Michelle Ulloa <umichelle244@gmail.com>
2023-05-27 16:49:22 -0400

    Added Drink, Order, Chip, and ReceiptFileManager classes

---
+++ src/main/java/com/yearup/deli/Drink.java
@@ -0,0 +1,47 @@
+package com.yearup.deli;
+
+import java.time.LocalDateTime;
+
+public class Drink extends Order{
+    private int drinkQuantity;
+    private String drinkType;
+
+    private double drinkPrice;
+
+    public Drink(int drinkQuantity, String drinkType,double drinkPrice) {
+        this.drinkQuantity = drinkQuantity;
+        this.drinkType = drinkType;
+        this.drinkPrice = drinkPrice;
+    }
+
+    public int getDrinkQuantity() {
+        return drinkQuantity;
+    }
+
+    public void setDrinkQuantity(int drinkQuantity) {
+        this.drinkQuantity = drinkQuantity;
+    }
+
+    public double getDrinkPrice() {
+        return drinkPrice;
+    }
+
+    public void setDrinkPrice(double drinkPrice) {
+        this.drinkPrice = drinkPrice;
+    }
+
+    public String getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(String drinkType) {
+        this.drinkType = drinkType;
+    }
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+    public boolean isFilled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/yearup/deli/Order.java
@@ -0,0 +1,9 @@
+package com.yearup.deli;
+
+public class Order {
+
+    public double getPrice() {
+        return 0;
+    }
+}
+
