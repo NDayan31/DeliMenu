6c24a8e38167164b58188f83ab00d42853dc2041
joonvent <gventura31@icloud.com>
2023-05-30 09:15:32 -0400

    Chips

--- src/main/java/com/yearup/deli/Chips.java
+++ src/main/java/com/yearup/deli/Chips.java
@@ -1,4 +1,40 @@
 package com.yearup.deli;
 
-public class Chips {
+public class Chips extends Order {
+
+    private String kindOfChips;
+    private int nummberofChips;
+
+    private double price;
+
+    public Chips(String kindOfChips, int nummberofChips, double price) {
+        this.kindOfChips = kindOfChips;
+        this.nummberofChips = nummberofChips;
+        this.price = price;
+    }
+
+    public String getKindOfChips() {
+        return kindOfChips;
+    }
+
+    public void setKindOfChips(String kindOfChips) {
+        this.kindOfChips = kindOfChips;
+    }
+
+    public int getNummberofChips() {
+        return nummberofChips;
+    }
+
+    public void setNummberofChips(int nummberofChips) {
+        this.nummberofChips = nummberofChips;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
