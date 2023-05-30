32d6db5745dde657f27a3a98afb58c4fca19b84b
Nadav Dayan <ndayan@appdev.yearup.org>
2023-05-30 09:46:41 -0400

    Added getters and setters the sandwich class

--- src/main/java/com/yearup/deli/Sandwich.java
+++ src/main/java/com/yearup/deli/Sandwich.java
@@ -1,4 +1,62 @@
 package com.yearup.deli;
 
-public class Sandwich {
+public class Sandwich extends Order{
+    private String breadType;
+    private int breadSize;
+    private boolean toasted;
+    private String premiumToppings;
+    private String basicToppings;
+    private double sandwichPrice;
+
+    public Sandwich(String breadType, int breadSize, boolean toasted, String premiumToppings, String basicToppings, double sandwichPrice) {
+        this.breadType = breadType;
+        this.breadSize = breadSize;
+        this.toasted = toasted;
+        this.premiumToppings = premiumToppings;
+        this.basicToppings = basicToppings;
+        this.sandwichPrice = sandwichPrice;
+    }
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public void setBreadType(String breadType) {
+        this.breadType = breadType;
+    }
+
+    public int getBreadSize() {
+        return breadSize;
+    }
+
+    public void setBreadSize(int breadSize) {
+        this.breadSize = breadSize;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        this.toasted = toasted;
+    }
+
+    public String getPremiumToppings() {
+        return premiumToppings;
+    }
+
+    public void setPremiumToppings(String premiumToppings) {
+        this.premiumToppings = premiumToppings;
+    }
+
+    public String getBasicToppings() {
+        return basicToppings;
+    }
+
+    public void setBasicToppings(String basicToppings) {
+        this.basicToppings = basicToppings;
+    }
+
+    public double getSandwichPrice() {
+        return sandwichPrice;
+    }
 }
