4c43927f1fca8b0bb7124f26c03f95dd1fbcd3e5
Nadav Dayan <ndayan@appdev.yearup.org>
2023-05-30 15:45:40 -0400

    fixed methods issue

--- src/main/java/com/yearup/deli/Menu.java
+++ src/main/java/com/yearup/deli/Menu.java
@@ -82,147 +82,151 @@ public class Menu {
             System.out.println("Would you like chips?");
 
         }
-    private static void addDrink(Scanner scanner) {
-        boolean exit = false;
-        String drinkSize = "";
-        String drinkType = "";
-        while (!exit) {
-            System.out.println("=============Drinks=============");
-            System.out.println("\t1. Coke");
-            System.out.println("\t2. Sprite");
-            System.out.println("\t3. Fruit Punch");
-            System.out.println("\t4. Fanta");
-            System.out.println("\t5. Ginger Ale");
-            System.out.println("\t6. Root Beer");
-            System.out.println("\t0. Return to main menu");
-            System.out.print("What kind of chips would you like: ");
-            int selection = scanner.nextInt();
-            switch (selection){
-                case 1: // Coke
-                    drinkType = "Coke";
-                    System.out.print("Enter size(S,M,L): ");
-                    drinkSize = scanner.nextLine();
-                    drinkSize(drinkType,drinkSize);
-                    break;
-                case 2: // Sprite
-                    drinkType = "Sprite";
-                    System.out.print("Enter size(S,M,L): ");
-                    drinkSize = scanner.nextLine();
-                    drinkSize(drinkType,drinkSize);
-                    break;
-                case 3: // Fruit Punch
-                    drinkType = "Fruit Punch";
-                    System.out.print("Enter size(S,M,L): ");
-                    drinkSize = scanner.nextLine();
-                    drinkSize(drinkType,drinkSize);
-                    break;
-                case 4: // Fanta
-                    drinkType = "Fanta";
-                    System.out.print("Enter size(S,M,L): ");
-                    drinkSize = scanner.nextLine();
-                    drinkSize(drinkType,drinkSize);
-                    break;
-                case 5: // Ginger Ale
-                    drinkType = "Ginger Ale";
-                    System.out.print("Enter size(S,M,L): ");
-                    drinkSize = scanner.nextLine();
-                    drinkSize(drinkType,drinkSize);
-                    break;
-                case 6: // Root Beer
-                    drinkType = "Root Beer";
-                    System.out.print("Enter size(S,M,L): ");
-                    drinkSize = scanner.nextLine();
-                    drinkSize(drinkType,drinkSize);
-                    break;
-                case 0:
-                    System.out.println("Returning to main menu");
-                    break;
-                default:
-                    System.out.println("Incorrect input");
-                    break;
+    }
+        private void addDrink (Scanner scanner){
+            boolean exit = false;
+            String drinkSize = "";
+            String drinkType = "";
+            while (!exit) {
+                System.out.println("=============Drinks=============");
+                System.out.println("\t1. Coke");
+                System.out.println("\t2. Sprite");
+                System.out.println("\t3. Fruit Punch");
+                System.out.println("\t4. Fanta");
+                System.out.println("\t5. Ginger Ale");
+                System.out.println("\t6. Root Beer");
+                System.out.println("\t0. Return to main menu");
+                System.out.print("What kind of chips would you like: ");
+                int selection = scanner.nextInt();
+                switch (selection) {
+                    case 1: // Coke
+                        drinkType = "Coke";
+                        System.out.print("Enter size(S,M,L): ");
+                        drinkSize = scanner.nextLine();
+                        drinkSize(drinkType, drinkSize);
+                        break;
+                    case 2: // Sprite
+                        drinkType = "Sprite";
+                        System.out.print("Enter size(S,M,L): ");
+                        drinkSize = scanner.nextLine();
+                        drinkSize(drinkType, drinkSize);
+                        break;
+                    case 3: // Fruit Punch
+                        drinkType = "Fruit Punch";
+                        System.out.print("Enter size(S,M,L): ");
+                        drinkSize = scanner.nextLine();
+                        drinkSize(drinkType, drinkSize);
+                        break;
+                    case 4: // Fanta
+                        drinkType = "Fanta";
+                        System.out.print("Enter size(S,M,L): ");
+                        drinkSize = scanner.nextLine();
+                        drinkSize(drinkType, drinkSize);
+                        break;
+                    case 5: // Ginger Ale
+                        drinkType = "Ginger Ale";
+                        System.out.print("Enter size(S,M,L): ");
+                        drinkSize = scanner.nextLine();
+                        drinkSize(drinkType, drinkSize);
+                        break;
+                    case 6: // Root Beer
+                        drinkType = "Root Beer";
+                        System.out.print("Enter size(S,M,L): ");
+                        drinkSize = scanner.nextLine();
+                        drinkSize(drinkType, drinkSize);
+                        break;
+                    case 0:
+                        System.out.println("Returning to main menu");
+                        break;
+                    default:
+                        System.out.println("Incorrect input");
+                        break;
+                }
+            }
+        }
+        private void drinkSize (String drinkType, String drinkSize){
+            if (drinkSize.equalsIgnoreCase("s")) {
+                //adds to orders array
+                System.out.println("Added to cart");
+            } else if (drinkSize.equalsIgnoreCase("m")) {
+                //adds to orders array
+                System.out.println("Added to cart");
+            } else if (drinkSize.equalsIgnoreCase("l")) {
+                //adds to orders array
+                System.out.println("Added to cart");
+            } else {
+                System.out.println("Incorrect Input");
+            }
+        }
+
+        private void addChips (Scanner scanner){
+            String chipType = "";
+            boolean exit = false;
+            while (!exit) {
+                System.out.println("=============Chips=============");
+                System.out.println("\t1. Lay's Potato Chips");
+                System.out.println("\t2. Tortilla Chips");
+                System.out.println("\t3. Lay's BBQ Chips");
+                System.out.println("\t4. Salt & Vinegar Chips");
+                System.out.println("\t5. Cheetos");
+                System.out.println("\t6. Doritos");
+                System.out.println("\t0. Return to main menu");
+                System.out.print("What kind of chips would you like: ");
+                int selection = scanner.nextInt();
+                switch (selection) {
+                    case 1: // Lay's Potato Chips
+                        chipType = "Lay's Potato Chips";
+                        System.out.println("Added to cart");
+                        //adds to orders array
+                        break;
+                    case 2: // Tortilla Chips
+                        chipType = "Tortilla Chips";
+                        System.out.println("Added to cart");
+                        //adds to orders array
+                        break;
+                    case 3: // Lay's BBQ Chips
+                        chipType = "Lay's BBQ Chips";
+                        System.out.println("Added to cart");
+                        //adds to orders array
+                        break;
+                    case 4: // Salt & Vinegar Chips
+                        chipType = "Salt & Vinegar Chips";
+                        System.out.println("Added to cart");
+                        //adds to orders array
+                        break;
+                    case 5: // Cheetos
+                        chipType = "Cheetos";
+                        System.out.println("Added to cart");
+                        //adds to orders array
+                        break;
+                    case 6: // Doritos
+                        chipType = "Doritos";
+                        System.out.println("Added to cart");
+                        //adds to orders array
+                        break;
+                    case 0:
+                        System.out.println("Returning to main menu");
+                        break;
+                    default:
+                        System.out.println("Incorrect input");
+                        break;
+                }
+            }
+        }
+
+        private void toppings () {
+            boolean running = true;
+            while (running) {
+                System.out.println("Meat:");
+                String meat = scanner.nextLine();
+                System.out.println("Extra meat?(y/n):");
+                String optionMeat = scanner.nextLine();
+                System.out.println("Extra meat:");
+                String extraMeat = scanner.nextLine();
             }
         }
     }
-    private static void drinkSize (String drinkType, String drinkSize) {
-        if (drinkSize.equalsIgnoreCase("s")){
-            //adds to orders array
-            System.out.println("Added to cart");
-        } else if (drinkSize.equalsIgnoreCase("m")) {
-            //adds to orders array
-            System.out.println("Added to cart");
-        } else if (drinkSize.equalsIgnoreCase("l")) {
-            //adds to orders array
-            System.out.println("Added to cart");
-        } else {System.out.println("Incorrect Input");}
-    }
 
-    private static void addChips(Scanner scanner) {
-        String chipType = "";
-        boolean exit = false;
-        while (!exit) {
-            System.out.println("=============Chips=============");
-            System.out.println("\t1. Lay's Potato Chips");
-            System.out.println("\t2. Tortilla Chips");
-            System.out.println("\t3. Lay's BBQ Chips");
-            System.out.println("\t4. Salt & Vinegar Chips");
-            System.out.println("\t5. Cheetos");
-            System.out.println("\t6. Doritos");
-            System.out.println("\t0. Return to main menu");
-            System.out.print("What kind of chips would you like: ");
-            int selection = scanner.nextInt();
-            switch (selection){
-                case 1: // Lay's Potato Chips
-                    chipType = "Lay's Potato Chips";
-                    System.out.println("Added to cart");
-                    //adds to orders array
-                    break;
-                case 2: // Tortilla Chips
-                    chipType = "Tortilla Chips";
-                    System.out.println("Added to cart");
-                    //adds to orders array
-                    break;
-                case 3: // Lay's BBQ Chips
-                    chipType = "Lay's BBQ Chips";
-                    System.out.println("Added to cart");
-                    //adds to orders array
-                    break;
-                case 4: // Salt & Vinegar Chips
-                    chipType = "Salt & Vinegar Chips";
-                    System.out.println("Added to cart");
-                    //adds to orders array
-                    break;
-                case 5: // Cheetos
-                    chipType = "Cheetos";
-                    System.out.println("Added to cart");
-                    //adds to orders array
-                    break;
-                case 6: // Doritos
-                    chipType = "Doritos";
-                    System.out.println("Added to cart");
-                    //adds to orders array
-                    break;
-                case 0:
-                    System.out.println("Returning to main menu");
-                    break;
-                default:
-                    System.out.println("Incorrect input");
-                    break;
-            }
-        }
-    }
-
-    private void toppings() {
-        boolean running = true;
-        while (running) {
-            System.out.println("Meat:");
-            String meat = scanner.nextLine();
-            System.out.println("Extra meat?(y/n):");
-            String optionMeat = scanner.nextLine();
-            System.out.println("Extra meat:");
-            String extraMeat = scanner.nextLine();
-        }
-    }
-}
 
 
 
