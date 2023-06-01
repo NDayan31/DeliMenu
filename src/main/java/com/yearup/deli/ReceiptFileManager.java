6404a9431e48a154ef9c8a6cd6c327d5c1803603
joonvent <gventura31@icloud.com>
2023-06-01 10:33:07 -0400

    Fixing file Manager

--- src/main/java/com/yearup/deli/ReceiptFileManager.java
+++ src/main/java/com/yearup/deli/ReceiptFileManager.java
@@ -1,8 +1,15 @@
 package com.yearup.deli;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class ReceiptFileManager {
     /*public static void main(String[] args) {*/
@@ -16,9 +23,32 @@ public class ReceiptFileManager {
             }
 
         } catch (IOException e) {
-
-
+            System.out.println("Folder Already Created");
+            return;
         }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime ldt = LocalDateTime.parse(now );
+
+        String fileName = now + ".txt";
+        Path filepath = receiptsFolder.resolve(fileName);
+
+        try {
+            FileWriter fw = new FileWriter(filepath.toFile());
+            fw.write("Help Me");
+            fw.close();
+            System.out.println("saved");
+
+        }catch (IOException e){
+            System.out.println("Failed");
+    }
+
+
+
+
+
+
 
     }
 }
